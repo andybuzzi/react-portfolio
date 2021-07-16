f366a9af5e1e09ae0c22b21bbeb6227f3b09d327
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-07-16 16:08:55 -0500

    resume component added

---
+++ src/components/Resume/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function Resume() {
+  return (
+    <section className="my-5">
+      <div className="container d-flex flex-column text-center">
+        <div className="p-2 text-center">
+          <h6 id="resume">
+            <a
+              href="https://drive.google.com/file/d/1R9ihLrLSYIcYrTj4zvmwdBxDQSGmcNgo/view?usp=sharing"
+              target="blank"
+              download
+            >
+              Click Here to Download Resume in PDF
+            </a>
+          </h6>
+        </div>
+        <br></br>
+        <ul>
+          Full-stack curriculum Bootcamp with emphasis on the most in-demand
+          technologies, including:
+          <br></br>
+          <br></br>
+          <li>- HTML</li>
+          <li>- CSS</li>
+          <li>- JavaScript</li>
+          <li>- jQuery</li>
+          <li>- Bootstrap</li>
+          <li>- Node.js</li>
+          <li>- MySQL</li>
+          <li>- MongoDB</li>
+          <li>- Express.js</li>
+          <li>- React</li>
+        </ul>
+      </div>
+      <div className="p-2"></div>
+    </section>
+  );
+}
+
+export default Resume;
